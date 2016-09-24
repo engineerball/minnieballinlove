c0b31575e2d344239d86694c064a457e6137e043
engineerball <ball@engineerball.com>
2016-09-24 18:18:30 +0700

    add db

--- process.php
+++ process.php
@@ -1,31 +1,37 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
+$servername = "127.0.0.1";
+$username = "minnieball";
+$password = "M1nnie<3bubbleball";
 $dbname = "minnieballinlove";
 
+if (isset($_POST['name']) && isset($_POST['mobile']) && isset($_POST['coming']) && isset($_POST['room']))
+{
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  $name = $_POST['name'];
+  $mobile = $_POST['mobile'];
+  $coming = $_POST['coming'];
+  $needroom = $_POST['room'];
+
+  $date = date('Y-m-d H:i:s');
 
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+
+  $sql = "INSERT INTO Guest (name, mobile, coming, room, create_date)
+  VALUES ($name, $mobile, $coming, $needroom, $date)";
+
+  if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+  } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  $conn->close();
 }
 
-$name = $_POST['name'];
-$mobile = $_POST['mobile'];
-$coming = $_POST['coming'];
-$needroom = $_POST['room'];
-
-$sql = "INSERT INTO MyGuests (name, mobile, coming, room)
-VALUES ($name, $mobile, $coming, $needroom)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
 ?>
