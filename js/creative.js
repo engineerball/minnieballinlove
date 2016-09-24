7b6e6e91d19c10c08f71bd1579f442b757d95a2d
engineerball <ball@engineerball.com>
2016-09-24 19:01:36 +0700

    fix js erro

--- js/creative.js
+++ js/creative.js
@@ -60,29 +60,4 @@
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
         }
     });
-
-    function myFunction() {
-      var name = document.getElementById("name").value;
-      var mobile = document.getElementById("mobile").value;
-      var coming = document.getElementById("coming").value;
-      var room = document.getElementById("room").value;
-      // Returns successful data submission message when the entered information is stored in database.
-      var dataString = 'name1=' + name + '&mobile1=' + mobile + '&coming1=' + coming + '&room1=' + room;
-      if (name == '' || mobile == '' || coming == '' || room == '') {
-      alert("Please Fill All Fields");
-      } else {
-      // AJAX code to submit form.
-      $.ajax({
-      type: "POST",
-      url: "process.php",
-      data: dataString,
-      cache: false,
-      success: function(html) {
-      alert(html);
-      }
-      });
-      }
-      return false;
-    }
-
 })(jQuery); // End of use strict
